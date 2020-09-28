98cfb8ed94f3c1347715de45e21af915446a933e
Jose Caraball <josecaraball@Joses-MacBook-Air.local>
2020-09-27 22:26:36 -0300

    debugging

--- Entrega2/php/index.php
+++ Entrega2/php/index.php
@@ -19,7 +19,7 @@
     <h3 align="center">Consulta 2: Buques de una naviera.</h3>
     <form align="center" action="queries/query2.php" method="post">
         <label for="naviera_nombre">Nombre de naviera:</label>
-        <input type="text" name="naviera_nombre" value=" ">
+        <input type="text" name="naviera_nombre">
         <br/>
         <input type="submit" value="Buscar">
     </form>
@@ -27,7 +27,7 @@
     <h3 align="center">Consulta 3: Buques que atracan en un puerto para un año.</h3>
     <form align="center" action="queries/query3.php" method="post">
         <label for="puerto_nombre">Nombre del puerto:</label>
-        <input type="text" name="puerto_nombre" value=" ">
+        <input type="text" name="puerto_nombre">
         <br/>
         <label for="año">Año:</label>
         <input type="number" name="año" min="1970" max="2099" value=2020>
@@ -38,10 +38,10 @@
     <h3 align="center">Consulta 4: Buques en un puerto al mismo tiempo que otro buque.</h3>
     <form align="center" action="queries/query4.php" method="post">
         <label for="puerto_nombre">Nombre del puerto:</label>
-        <input type="text" name="puerto_nombre" value=" ">
+        <input type="text" name="puerto_nombre">
         <br/>
         <label for="buque_nombre">Nombre del buque:</label>
-        <input type="text" name="buque_nombre" value=" ">
+        <input type="text" name="buque_nombre">
         <br/>
         <input type="submit" value="Buscar">
     </form>
@@ -54,7 +54,7 @@
         <input type="radio" name="genero" value="hombre">
         <br/>
         <label for="puerto_nombre">Nombre del puerto:</label>
-        <input type="text" name="puerto_nombre" value=" ">
+        <input type="text" name="puerto_nombre">
         <br/>
         <input type="submit" value="Buscar">
     </form>
